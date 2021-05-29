0ea9b7238a3c1f6c029772f58c2d382e27b0fbca
Bleakney Bissett <bleakneybissett@gmail.com>
2021-05-29 18:14:21 -0500

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,72 @@
 // Assignment code here
 
+var getCharacterCount = function() {
+ var characterCount = window.prompt("How many characters would you like your password to have? You must choose at least 8 and no more than 128.");
+ // while loop to prevent user from generating a password of inappropriate length
+ if (characterCount < 8 || characterCount > 128 || isNaN(characterCount))  {
+   window.alert("Invalid number. Please enter a number between 8 and 128.");
+   return getCharacterCount();
+ }
+ console.log(characterCount);
+ return characterCount;
+}
+
+var characterCount = getCharacterCount();
+
+// prompts to see which characters to include in password: lowercase, uppercase, numbers, special characters
+
+var characterTypes = {
+  lowercase: window.confirm("Would you like to include lowercase characters in your password?"),
+  uppercase: window.confirm("Would you like to include uppercase characters in your password?"),
+  numericals: window.confirm("Would you like to include number characters in your password?"),
+  special: window.confirm("Would you like to include special characters in your password?")
+}
+
+console.log(characterTypes.lowercase);
+console.log(characterTypes.uppercase);
+console.log(characterTypes.numericals);
+console.log(characterTypes.special);
+
+var passwordCharacters = function() {
+  if (characterTypes.lowercase === true) {
+    lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";}
+    else {
+      lowercaseCharacters = ""};
+  if (characterTypes.uppercase === true) {
+    uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else {
+    uppercaseCharacters = ""
+  }
+  if (characterTypes.special === true) {
+    specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  } else {
+    specialCharacters = ""
+  }
+  if (characterTypes.numericals === true) {
+    numberCharacters = "0123456789";
+  } else {
+    numberCharacters = "";
+  }
+  return lowercaseCharacters + uppercaseCharacters + specialCharacters + numberCharacters;
+  }
+
+  var chosenCharacters = passwordCharacters();
+
+ //if (characterTypes.lowercase === true) {
+  // lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)]}
+
+function generatePassword() {
+  var generatedPassword = "";
+  var i = 0;
+  while (i < characterCount) {
+    generatedPassword += chosenCharacters[Math.floor(Math.random()* chosenCharacters.length)];
+    i++;
+  }
+  return generatedPassword;
+}
+  // create object to store user's password criteria
+ 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
